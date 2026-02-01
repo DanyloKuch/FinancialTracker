12965e1afa92e51fa41a09e92b04b543be4f7fe2
DanyloKuch <kucherudanylo@gmail.com>
2026-02-01 19:20:19 +0200

    Comiit

--- FinancialTracker/FinancialTracker.Web/Models/CategoryDto.cs
+++ FinancialTracker/FinancialTracker.Web/Models/CategoryDto.cs
@@ -2,7 +2,8 @@
 {
     public class CategoryDto
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
+        public decimal TotalLimit { get; set; }
     }
 }
--- FinancialTracker/FinancialTracker.Web/Models/TransactionDto.cs
+++ FinancialTracker/FinancialTracker.Web/Models/TransactionDto.cs
@@ -7,5 +7,6 @@
         public int Type { get; set; } // 0=Income, 1=Expense, 2=Transfer
         public string Comment { get; set; } = "";
         public DateTime CreatedAt { get; set; }
+        public Guid? CategoryId { get; set; }
     }
 }
