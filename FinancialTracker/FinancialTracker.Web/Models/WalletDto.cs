29ff3b5543f60da5c2bd00c27335e89cd7678600
Yuliia Chastukhina <yulia_chastuhina@knu.ua>
2026-02-01 22:50:19 +0200

    Implement wallet management logic and enhance modal UI design

--- FinancialTracker/FinancialTracker.Web/Models/WalletDto.cs
+++ FinancialTracker/FinancialTracker.Web/Models/WalletDto.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public decimal Balance { get; set; }
         public string CurrencyCode { get; set; }
+        public string Type { get; set; }
     }
 }
